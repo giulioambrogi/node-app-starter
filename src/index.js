80031204d2636a9f00bba5beedd3989ecaacb78c
Giulio Ambrogi <giulio.ambrogi@gmail.com>
2019-12-29 09:38:58 +0000

    FIRST commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { add } from './lib/utils';
+
+const main = () => {
+  console.log('Hello world', add(1, 2));
+};
+
+main();
---
+++ src/lib/__tests__/utils.test.js
@@ -0,0 +1,7 @@
+import { add } from '../utils';
+
+describe('add', () => {
+  it('adds 2 numbers correctly', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
